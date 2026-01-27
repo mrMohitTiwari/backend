6aa0d6259ac595f993e84048c9ef130977a42a40
Mohit Tiwari <mt0988145@gmail.com>
2026-01-27 22:21:41 +0530

    updating files

--- mongoose/ConncectingFirst.js
+++ mongoose/ConncectingFirst.js
@@ -1,2 +1,2 @@
-const mongoose =require("mongoose");
-mongoose.connect("mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/")
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/");
