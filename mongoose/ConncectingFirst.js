fc4a7ccd64f90423a53e96bc30d8e3b5440b77f3
Mohit Tiwari <mt0988145@gmail.com>
2026-01-30 22:43:12 +0530

    updatinfiles

--- mongoose/ConncectingFirst.js
+++ mongoose/ConncectingFirst.js
@@ -1,2 +1,6 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/userappnew').then(() => console.log("mongoose connected Suceefully")).catch(() => console.log("Error while connecting to Server"))
+
+const Cat = mongoose.model('Cat', { name: String });
+const kitty = new Cat({ name: "Zildijan" });
+kitty.save().then(() => console.log("meow"))
